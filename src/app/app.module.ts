a3c5ef5d2e6421d84f71a1b9ad4046d833026b42
Bafedile <Ddimakatso30@gmail.com>
2023-01-27 16:12:47 +0200

    resolved the issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,12 +47,6 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     ReactiveFormsModule,
     HttpClientModule,
     FormsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
   ],
   providers: [AnimalService,
     AnimalGuardService,  DropdownDirective, 
